66d06cbd176d3cf0b23e7b5cbccb95ec8b02a07e
kousar1234 <kousar.themelooks@gmail.com>
2023-09-12 12:36:57 +0600

    modify composer and core service provider

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -25,7 +25,31 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadCoreHelpers();
         $this->loadViewsFrom(base_path('Core/resources/views'), 'core');
         View::composer(['core::base.layouts.header', 'core::base.layouts.master', 'core::base'], Core::class);
+    }
+
+    public function loadCoreHelpers()
+    {
+        if (file_exists(base_path('Core/Helpers/Helpers.php'))) {
+            require_once(base_path('Core/Helpers/Helpers.php'));
+        }
+
+        if (file_exists(base_path('Core/Helpers/Theme.php'))) {
+            require_once(base_path('Core/Helpers/Theme.php'));
+        }
+
+        if (file_exists(base_path('Core/Helpers/Plugin.php'))) {
+            require_once(base_path('Core/Helpers/Plugin.php'));
+        }
+
+        if (file_exists(base_path('Core/Helpers/Notification.php'))) {
+            require_once(base_path('Core/Helpers/Notification.php'));
+        }
+
+        if (file_exists(base_path('Core/Helpers/Media.php'))) {
+            require_once(base_path('Core/Helpers/Media.php'));
+        }
     }
 }
