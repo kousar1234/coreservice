9e7f7f20d45abb199ec4d53734d5473ebcbfe306
kousar1234 <kousar.themelooks@gmail.com>
2023-09-12 14:12:53 +0600

    add core namespace loader

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace ThemeLooks\CoreService;
 
+use Composer\Autoload\ClassLoader;
 use Illuminate\Support\Facades\View;
 use Core\Views\Composer\Core;
 use Illuminate\Support\ServiceProvider;
@@ -25,9 +26,17 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadCoreNamespace();
         $this->loadCoreHelpers();
         $this->loadViewsFrom(base_path('Core/resources/views'), 'core');
         View::composer(['core::base.layouts.header', 'core::base.layouts.master', 'core::base'], Core::class);
+    }
+
+    public function loadCoreNamespace()
+    {
+        $loader = new ClassLoader;
+        $loader->setPsr4('Core\\', base_path('Core'));
+        $loader->register(true);
     }
 
     public function loadCoreHelpers()
