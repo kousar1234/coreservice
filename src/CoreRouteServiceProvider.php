35274a192824716dbd7e34758f5e86093fa11135
kousar1234 <kousar.themelooks@gmail.com>
2023-09-12 17:49:04 +0600

    modify service providers

--- src/CoreRouteServiceProvider.php
+++ src/CoreRouteServiceProvider.php
@@ -24,12 +24,56 @@ class CoreRouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->routes(function () {
-            Route::middleware('web')
-                ->prefix(getAdminPrefix())
-                ->group(base_path('Core/routes/core.php'));
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('Core/routes/api.php'));
+            $this->RegisterCoreRoute();
+            if (env('IS_USER_REGISTERED') == 1) {
+                $this->registerPluginRoute();
+                $this->registerThemeRoute();
+            }
         });
+    }
+
+    public function RegisterCoreRoute()
+    {
+        Route::middleware('web')
+            ->prefix(getAdminPrefix())
+            ->group(base_path('Core/routes/core.php'));
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('Core/routes/api.php'));
+    }
+
+
+    public function registerPluginRoute()
+    {
+        $plugins = getActivePlugins();
+        foreach ($plugins as $plugin) {
+            if (file_exists(base_path('plugins/' . $plugin->location . '/routes/api.php'))) {
+                Route::middleware('api')
+                    ->prefix('api')
+                    ->group(base_path('plugins/' . $plugin->location . '/routes/api.php'));
+            }
+
+            if (file_exists(base_path('plugins/' . $plugin->location . '/routes/web.php'))) {
+                Route::middleware('web')
+                    ->group(base_path('plugins/' . $plugin->location . '/routes/web.php'));
+            }
+        }
+    }
+
+    public function registerThemeRoute()
+    {
+        $active_theme = getActiveTheme();
+        if ($active_theme != null) {
+            if (file_exists(base_path('themes/' . $active_theme->location . '/routes/api.php'))) {
+                Route::middleware('api')
+                    ->prefix('api')
+                    ->group(base_path('themes/' . $active_theme->location . '/routes/api.php'));
+            }
+
+            if (file_exists(base_path('themes/' . $active_theme->location . '/routes/web.php'))) {
+                Route::middleware('web')
+                    ->group(base_path('themes/' . $active_theme->location . '/routes/web.php'));
+            }
+        }
     }
 }
--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -37,6 +37,10 @@ class CoreServiceProvider extends ServiceProvider
         $this->loadCoreNamespace();
         $this->loadCoreHelpers();
         $this->loadViewsFrom(base_path('Core/resources/views'), 'core');
+        if (env('IS_USER_REGISTERED') == 1) {
+            $this->registerPlugins();
+            $this->registerTheme();
+        }
         View::composer(['core::base.layouts.header', 'core::base.layouts.master', 'core::base'], Core::class);
     }
 
@@ -54,6 +58,56 @@ class CoreServiceProvider extends ServiceProvider
             if (file_exists($helperFile)) {
                 require_once($helperFile);
             }
+        }
+    }
+
+    public function registerPlugins()
+    {
+        $plugins = getActivePlugins();
+
+        foreach ($plugins as $plugin) {
+            //Merge config
+            $has_config = file_exists(base_path('plugins/' . $plugin->location . '/config/config.php'));
+            if ($has_config) {
+                $this->mergeConfigFrom(base_path('plugins/' . $plugin->location . '/config/config.php'), $plugin->location);
+            }
+            //Load helper
+            $has_helpers = file_exists(base_path('plugins/' . $plugin->location . '/helpers/helpers.php'));
+            if ($has_helpers) {
+                require_once(base_path('plugins/' . $plugin->location . '/helpers/helpers.php'));
+            }
+            //Load view
+            $this->loadViewsFrom(base_path('plugins/' . $plugin->location . '/views'), 'plugin/' . $plugin->location);
+
+            //Generate Namespace
+            $loader = new ClassLoader;
+            $loader->setPsr4($plugin->namespace, base_path('plugins/' . $plugin->location . '/src'));
+            $loader->register(true);
+        }
+    }
+
+    public function registerTheme()
+    {
+        $active_theme = getActiveTheme();
+        if ($active_theme != null) {
+            //Merge config
+            $has_config = file_exists(base_path('themes/' . $active_theme->location .  '/config/config.php'));
+            if ($has_config) {
+                $this->mergeConfigFrom(base_path('themes/' . $active_theme->location .  '/config/config.php'), $active_theme->location);
+            }
+
+            //Load helper functions
+            $has_helpers = file_exists(base_path('themes/' . $active_theme->location . '/helpers/helpers.php'));
+            if ($has_helpers) {
+                require_once(base_path('themes/' . $active_theme->location . '/helpers/helpers.php'));
+            }
+
+            //Generate namespace
+            $loader = new ClassLoader;
+            $loader->setPsr4($active_theme->namespace, base_path('themes/' . $active_theme->location . '/src'));
+            $loader->register(true);
+            //Load view
+            $this->loadViewsFrom(base_path('themes/' . $active_theme->location . '/resources/views'), 'theme/' . $active_theme->location);
         }
     }
 }
